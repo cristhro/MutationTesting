fc1d578a171991c8470cfdc1687364349f7b886b
cristhian rodriguez <cristhian@footuro.es>
2018-01-11 21:20:33 +0100

    Eliminacion de archivos

--- MutationTree/MutationTree/src/data/ICustomFileWrite.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-public interface ICustomFileWrite {
-	/** Cierra el fichero de escritura */
-	public abstract void closeFileWrite(CustomFileWrite file);
-	/** Escribe en el fichero de escritura */
-	public abstract void writeFile(CustomFileWrite file, String text);
-}
