c203f15023457bfa5789279e9b27d728538c7301
yuliu03@ucm.es <yuliu03@ucm.es>
2018-04-14 15:15:10 +0200

    generador de programas

---
+++ src/Try.java
@@ -0,0 +1,12 @@
+
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="abc";
+		StringBuffer s1=new StringBuffer("qwe");
+		s+=s1;
+		System.out.println(s);
+	}
+
+}
